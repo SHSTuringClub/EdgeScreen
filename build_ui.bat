0e189b9fdd8b25c679d8bcab6b19fa1cb7c69a1c
invisiblearts <invisiblearts@outlook.com>
2016-02-27 10:58:34 +0800

    UI minor changes, build script all-in-one

--- build_ui.bat
+++
@@ -1,3 +0,0 @@
-pyuic5 ui/entrance.ui -o ui_ent.py
-pyuic5 ui/horizontal.ui -o ui_hor.py
-pyuic5 ui/vertical.ui -o ui_ver.py
--- build_ui.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-pyuic5 ui/entrance.ui -o ui_ent.py
-pyuic5 ui/horizontal.ui -o ui_hor.py
-pyuic5 ui/vertical.ui -o ui_ver.py
